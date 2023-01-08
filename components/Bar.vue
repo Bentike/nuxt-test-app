397cc24a73dc0df83df446eede85ec1d044cc1b5
Bentike <ajadiyusuf73@gmail.com>
2023-01-08 10:17:29 +0100

    designed Bar component

--- components/Bar.vue
+++ components/Bar.vue
@@ -1,5 +1,9 @@
 <template>
-
+    <div class="flex justify-center">
+        <img class="mt-5 ml-1 w-4" src="~/assets/images/one.png" />
+        <img class="mt-6 ml-1 w-2 h-2" src="~/assets/images/two.png" />
+        <img class="mt-6 ml-1 w-2 h-2" src="~/assets/images/three.png" />
+    </div>
 </template>
 
 <script>
--- components/SignIn.vue
+++ components/SignIn.vue
@@ -4,6 +4,7 @@
              <div class="w-4/6 text-center m-auto">
                  <h1 class="text-white">Welcome Back</h1>
                  <p class="text-white">Just a couple of clicks and we start</p>
+                 <Bar/>
              </div>
         </div>
         <div class="w-1/2 bg-white  h-screen flex items-center justify-center">
