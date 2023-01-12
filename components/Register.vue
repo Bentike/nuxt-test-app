b14dde7ac0b58178741878ea76c12c83e4eee791
Bentike <ajadiyusuf73@gmail.com>
2023-01-12 22:08:33 +0100

    aligned inputs in register page

--- components/Register.vue
+++ components/Register.vue
@@ -13,12 +13,33 @@
              <div class="w-4/6 text-center m-auto">
                  <h1 class="text-4xl text-myBlue mb-5 place-self-start">Create Account</h1>
                  <form>
+                    <div class="flex w-full justify-between">
+                        <div class="w-[47%]">
+                            <label class="text-myBlue font-medium inline-block w-full text-start"> FirstName
+                                <input class="w-full p-3 bg-inputGray" type="text" placeholder="John">
+                            </label>
+                        </div>
+                        <div class="w-[47%]">
+                            <label  class="text-myBlue font-medium inline-block w-full text-start"> LastName
+                                <input class="w-full p-3 bg-inputGray" type="text" placeholder="Doe">
+                            </label>
+                        </div>
+                    </div> <br>
                     <label class="text-myBlue font-medium inline-block w-full text-start">Email <br/>
                         <input class="w-full p-3 bg-inputGray" type="email" placeholder="johndoe@gmail.com"/>
                     </label> <br/><br/>
-                    <label class="text-myBlue font-medium inline-block w-full text-start">Password <br/>
-                        <input class="w-full p-3 bg-inputGray" type="password" placeholder="***************"/>
-                    </label> <br/><br/>
+                    <div class="flex w-full justify-between">
+                        <div class="w-[47%]">
+                            <label class="text-myBlue font-medium inline-block w-full text-start"> Password
+                                <input class="w-full p-3 bg-inputGray" type="password" placeholder="**********">
+                            </label>
+                        </div>
+                        <div class="w-[47%]">
+                            <label  class="text-myBlue font-medium inline-block w-full text-start"> Confirm Password
+                                <input class="w-full p-3 bg-inputGray" type="passwrod" placeholder="**********">
+                            </label>
+                        </div>
+                    </div>   <br/><br/>
                     <button class="w-full bg-btnYellow p-3 mb-3">Sign Up</button>
                     <p><span class="opacity-60">Don’t have an account?</span> <span class="text-lightBlue">Sign In</span></p>
                  </form>
