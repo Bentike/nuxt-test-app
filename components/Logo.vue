c76f52c305918c555c4acef2d84ec5860825b084
Bentike <ajadiyusuf73@gmail.com>
2023-01-13 12:24:59 +0100

    changed logo image

--- components/Logo.vue
+++ components/Logo.vue
@@ -1,11 +1,7 @@
 <template>
     <div class="flex justify-end relative">
-        <h1 class="absolute lg:top-[100px] lg:right-[60px] sm:right-[40px] sm:top-[50px]
-           text-4xl tracking-wide text-myBlue">
-            SAPP
-        </h1>
-        <img src="~/assets/images/logo.png" />
-    </div>
+    <img src="~/assets/images/logo.png" />
+    </div>   
 </template>
 
 <script>
